aebfc70f90199dc926d0bfbf5cef7327c1f693dd
Dhanny Adhi Pramana <p.dhannypramana@gmail.com>
2023-11-25 02:41:33 +0700

    fix [reports] code optimation/refactoring on reports controller

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -18,94 +18,79 @@ class ReportController extends Controller
         ]);
     }
 
+    function buildBaseQuery($model, $request)
+    {
+        $query = $model::whereHas('user', function ($query) use ($request) {
+            if ($request->faculty !== 'all_faculty') {
+                $query->where('major', '=', $request->faculty);
+            }
+            if ($request->academic_program !== 'all_academic_program') {
+                $query->where('academic_program', '=', $request->academic_program);
+            }
+        })->with(['user']);
+
+        return $query;
+    }
+
+    function applyDateFilter($query, $request)
+    {
+        if ($request->useDate === 'true') {
+            $query->where('created_at', '>=', $request->start_date)
+                ->where('created_at', '<=', $request->end_date);
+        }
+
+        return $query;
+    }
+
+    function applyLocationFilter($query, $request, $locationField, $allLocationField)
+    {
+        if ($request->$locationField !== $allLocationField) {
+            $query->where('location', $request->$locationField);
+        }
+
+        return $query;
+    }
+
     function getFilters(Request $request)
     {
         switch ($request->service) {
             case 'penelitian':
-                $data = Research::whereHas('user', function ($query) use ($request) {
-                    if ($request->faculty !== 'all_faculty') {
-                        $query->where('major', '=', $request->faculty);
-                    }
-                    if ($request->academic_program !== 'all_academic_program') {
-                        $query->where('academic_program', '=', $request->academic_program);
-                    }
-                })->with(['user']);
-
-                if ($request->researchLocation !== 'all_location') {
-                    $data = $data->where('location', $request->researchLocation);
-                }
-
-                if ($request->useDate === 'true') {
-                    $data = $data->where('created_at', '>=', $request->start_date)->where('created_at', '<=', $request->end_date);
-                }
-
-                $data = $data->get();
+                $query = $this->buildBaseQuery(Research::class, $request);
+                $query = $this->applyLocationFilter($query, $request, 'researchLocation', 'all_location');
+                $query = $this->applyDateFilter($query, $request);
                 break;
+
             case 'permintaanData':
-                $data = DataRequest::whereHas('user', function ($query) use ($request) {
-                    if ($request->faculty !== 'all_faculty') {
-                        $query->where('major', '=', $request->faculty);
-                    }
-                    if ($request->academic_program !== 'all_academic_program') {
-                        $query->where('academic_program', '=', $request->academic_program);
-                    }
-                })->with(['user']);
-
-                if ($request->useDate === 'true') {
-                    $data = $data->where('created_at', '>=', $request->start_date)->where('created_at', '<=', $request->end_date);
-                }
-
-                $data = $data->get();
+                $query = $this->buildBaseQuery(DataRequest::class, $request);
+                $query = $this->applyDateFilter($query, $request);
                 break;
 
             case 'peminjaman':
-                $data = Loan::whereHas('user', function ($query) use ($request) {
-                    if ($request->faculty !== 'all_faculty') {
-                        $query->where('major', '=', $request->faculty);
-                    }
-                    if ($request->academic_program !== 'all_academic_program') {
-                        $query->where('academic_program', '=', $request->academic_program);
-                    }
-                })->with(['user']);
-
+                $query = $this->buildBaseQuery(Loan::class, $request);
                 if ($request->loanCategory !== 'all_category') {
                     if ($request->loanCategory == 'Lain-Lain') {
-                        $data = $data->where('category', '<>', 'Ruangan')->where('category', '<>', 'Alat');
+                        $query->where('category', '<>', 'Ruangan')->where('category', '<>', 'Alat');
                     } else {
-                        $data = $data->where('category', $request->loanCategory);
+                        $query->where('category', $request->loanCategory);
                     }
                 }
-
-                if ($request->useDate === 'true') {
-                    $data = $data->where('created_at', '>=', $request->start_date)->where('created_at', '<=', $request->end_date);
-                }
-
-                $data = $data->get();
+                $query = $this->applyDateFilter($query, $request);
                 break;
 
             case 'praktikum':
-                $data = Practicum::whereHas('user', function ($query) use ($request) {
-                    if ($request->faculty !== 'all_faculty') {
-                        $query->where('major', '=', $request->faculty);
-                    }
-                    if ($request->academic_program !== 'all_academic_program') {
-                        $query->where('academic_program', '=', $request->academic_program);
-                    }
-                })->with(['user']);
-
-                if ($request->practicumLocation !== 'all_location_practicum') {
-                    $data = $data->where('location', $request->practicumLocation);
-                }
-
-                if ($request->useDate === 'true') {
-                    $data = $data->where('created_at', '>=', $request->start_date)->where('created_at', '<=', $request->end_date);
-                }
-
-                $data = $data->get();
+                $query = $this->buildBaseQuery(Practicum::class, $request);
+                $query = $this->applyLocationFilter($query, $request, 'practicumLocation', 'all_location_practicum');
+                $query = $this->applyDateFilter($query, $request);
                 break;
+
             default:
-                break;
+                return response()->json([
+                    'data' => [],
+                ]);
         }
+
+        $data = $query->get();
+
         return response()->json([
             'data' => $data,
         ]);
